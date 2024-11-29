2b456b29e9e542cfd05d9181e3bbaa188de9c160
Tiago Gomes <tidgomes@gmail.com>
2024-11-29 12:31:39 -0300

    chore: install and configure eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import globals from "globals"
+import pluginJs from "@eslint/js"
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    rules: {
+      "no-unused-vars": ["error", "off"],
+      indent: ["error", 2],
+      quotes: [2, "double"],
+      "eol-last": 1,
+      semi: ["error", "never"],
+      "object-curly-spacing": ["error","always"]
+    }
+  },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+]
